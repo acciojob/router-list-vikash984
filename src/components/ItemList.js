6a50c3c7f2446dfa7da557d8655aa0ce8b205ce3
vikash984 <vikashk15072001@gmail.com>
2025-08-03 16:28:44 +0530

    Create ItemList.js

---
+++ src/components/ItemList.js
@@ -0,0 +1,21 @@
+
+import React from "react";
+import { Link } from "react-router-dom";
+import items from "./data";
+
+const ItemList = () => {
+  return (
+    <div>
+      <h1>Item List</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <Link to={`/items/${item.id}`}>{item.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ItemList;
