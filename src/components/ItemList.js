8146af300ef78a0ecdd5bcf3c248f8e56948fb67
vikash984 <vikashk15072001@gmail.com>
2025-08-03 16:47:39 +0530

    Update ItemList.js

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,9 +1,13 @@
-
 import React from "react";
 import { Link } from "react-router-dom";
-import items from "./data";
 
-const ItemList = () => {
+const items = [
+  { id: 1, name: "Item 1", description: "Description for Item 1" },
+  { id: 2, name: "Item 2", description: "Description for Item 2" },
+  { id: 3, name: "Item 3", description: "Description for Item 3" }
+];
+
+function ItemList() {
   return (
     <div>
       <h1>Item List</h1>
@@ -16,6 +20,6 @@ const ItemList = () => {
       </ul>
     </div>
   );
-};
+}
 
 export default ItemList;
